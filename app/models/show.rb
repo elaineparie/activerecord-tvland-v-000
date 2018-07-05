95b89e92af2439c8a561490b7c0ed3fa80c7a196
Elaine Anderson <eparie@bu.edu>
2018-07-05 21:59:24 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,6 +2,8 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
 
-  def self.build_network
-  end 
+  def self.build_network(letters)
+    self.call_letters = letters
+
+  end
 end
